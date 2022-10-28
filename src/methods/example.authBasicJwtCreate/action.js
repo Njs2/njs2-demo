f3999d0c172ed52e055c7881e8579b05c574c46d
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-10-28 13:22:14 +0530

    Update action.js

--- src/methods/example.authBasicJwtCreate/action.js
+++ src/methods/example.authBasicJwtCreate/action.js
@@ -1,20 +1,6 @@
 const jwt = require("jsonwebtoken");
 class ExampleAuthBasicJwtCreateAction extends baseAction {
   async executeMethod() {
-    /*
-    AUTHENTICATION TOKEN GENERATION USING JWT EXAMPLE
-
-    Description:
-    -This example will generate access_token for your user_id argument and will send in response.
-
-    Steps:
-    1. Install jsonwebtoken package: npm install jsonwebtoken --save
-    2. Import: const jwt = require('jsonwebtoken');
-    3. Send user_id as an argument to this action from postman or visit following URL in Browser,
-        http://localhost:3000/example/authBasicJwtCreate?user_id=123
-    4. Save or copy the access_token for further use.
-    */
-
     let { user_id } = this;
     let access_token = jwt.sign({ user_id }, "my_secret_key");
     this.setResponse("SUCCESS");
