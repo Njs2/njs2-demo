e26835d27f86c22e5ace29f616981c4f6061c61f
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-05 14:28:59 +0530

    Update action.js

--- src/methods/example.cron/action.js
+++ src/methods/example.cron/action.js
@@ -1,13 +1,6 @@
 const fs = require("fs");
 class ExampleCronAction extends baseAction {
   async executeMethod() {
-    /*
-    CRON EXAMPLE
-    /*
-    Description : This module is used to execute business logic which requires to be executed at time interval more than a minute .
-    1. Business logic should be written inside the tasks folder with naming convetions as per the defualt example file(cron.tasks.js).
-    2. Enabling/Disabling the Cron job can be done in the config.json file inside SCHEDULER key.    
-    */
     this.setResponse("SUCCESS");
     return {};
   }
