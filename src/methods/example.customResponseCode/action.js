c2493fa863c51185a423b360aeb38415a6b34161
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-05 14:34:30 +0530

    Update action.js

--- src/methods/example.customResponseCode/action.js
+++ src/methods/example.customResponseCode/action.js
@@ -1,27 +1,5 @@
 class ExampleCustomResponseCodeAction extends baseAction {
   async executeMethod() {
-    /*
-    CUSTOM RESPONSE EXAMPLE
-
-    Description:
-    -This example explains how to define and use the custom response, similar to "SUCCESS".
-
-    Steps:
-    1. Update below content in global/i18n/response.js.
-        INVALID_USER: {
-          responseCode: 1001, responseMessage: {
-            "en": "Invalid user data"
-          }
-        },
-        CUSTOM_RESPONSE: {
-          responseCode: 1002, responseMessage: {
-            "en": "Custom response"
-          }
-        },  
-    2. Send the request to the following URL:
-          http://localhost:3000/example/customResponse
-    */
-
     this.setResponse("CUSTOM_RESPONSE");
     return {};
   }
