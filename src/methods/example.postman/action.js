84a105e405c4ffc00d625619e87f99585619198c
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-08 10:25:15 +0530

    Update action.js

--- src/methods/example.postman/action.js
+++ src/methods/example.postman/action.js
@@ -1,20 +1,6 @@
 class ExamplePostmanAction extends baseAction {
   async executeMethod() {
-    /*
-    POSTMAN USAGE EXAMPLE
-
-    Steps:
-    1. Download Postman in your machine.
-    2. In Postman click on import -> select link tab -> paste "http://localhost:3000/postman" to import collection.
-    3. Select demo-project from collection and click on example/postman API.
-    4. Under the params section, add the following parameters:
-      - x: 10
-      - y: hello
-    5. Hit send button.
-    */
-
     let { x, y } = this;
-
     this.setResponse("SUCCESS");
     return `Arguments recieved. First Argument=${x}, Second Argument=${y}`;
   }
