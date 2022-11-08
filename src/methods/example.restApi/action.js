141d7176bf2f0ea7583ac892c270c235430d86de
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-08 10:36:04 +0530

    Update action.js

--- src/methods/example.restApi/action.js
+++ src/methods/example.restApi/action.js
@@ -2,23 +2,7 @@
 class ExampleRestApiAction extends baseAction {
 
   async executeMethod() {
-    /*
-    REST API EXAMPLE
-
-    Description:
-    - This example shows how to define a rest api.
-    - By default in init.js, Method is set to GET method. So it works with GET method.
-
-    Steps:
-    In init.js set the following:
-    1. To create GET request, set this.initializer.requestMethod = ["GET"]
-    2. To create POST request, set this.initializer.requestMethod = ["POST"]
-    3. To create PUT request, set this.initializer.requestMethod = ["PUT"]
-    4. To create DELETE request, set this.initializer.requestMethod = ["DELETE"]
-    5. To support multiple request methods, set this.initializer.requestMethod = ["GET", "POST", "PUT", "DELETE"]
-    */
     let { user_id } = this;
-
     this.setResponse('SUCCESS');
     return {user_id};
   };
