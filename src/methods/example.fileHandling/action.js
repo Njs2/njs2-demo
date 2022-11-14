25524a4036914b46f95b5f11955172364ebb7d76
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 06:41:34 +0530

    Update action.js

--- src/methods/example.fileHandling/action.js
+++ src/methods/example.fileHandling/action.js
@@ -2,19 +2,7 @@ const fs = require("fs");
 
 class ExampleFileHandlingAction extends baseAction {
   async executeMethod() {
-    /*
-    FILE UPLOAD IN LOCAL EXAMPLE
-
-    Description:
-    -This example will store the file in Assets/Uploads folder.
-    -File location will be sent in the response.
-
-    Steps:
-    1. In init.js, define a parameter as type: 'file', please check the init.js file.
-    2. Import 'fs' module for file manipulation.
-    3. In postman under body tab, under form-data section you can upload file.
-    */
-
+  
     let { fileObject } = this;
 
     const path = process.cwd() + "/Assets/Uploads/" + fileObject.filename;
