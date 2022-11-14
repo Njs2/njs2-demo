589414b9130e3940c5ee46a33e1d7451eb2936da
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 06:53:50 +0530

    Update action.js

--- src/methods/example.global/action.js
+++ src/methods/example.global/action.js
@@ -1,13 +1,5 @@
 class ExampleGlobalAction extends baseAction {
   async executeMethod() {
-    /*
-    GLOBAL CONSTANTS EXAMPLE
-    
-    Steps:
-    1. Add "constant.FRAMEWORK_NAME = "NJS2" statement to global/constants.js file.
-    2. By adding this, It's scope is global, i.e it will available throughout the project without importing or exporting it.
-    3. To access it anywhere, you can use GLB.FRAMEWORK_NAME.
-    */
 
     const framework_name = GLB.FRAMEWORK_NAME;
     this.setResponse("SUCCESS");
