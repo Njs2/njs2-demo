44f022cd27b513b0b07bf4b0d0420657114c1257
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 10:31:47 +0530

    Update action.js

--- src/methods/example.mongoDeleteUser/action.js
+++ src/methods/example.mongoDeleteUser/action.js
@@ -1,16 +1,6 @@
 class ExampleMongoDeleteUserAction extends baseAction {
   async executeMethod() {
-    /*
-    MONGO-DB DELETE USER EXAMPLE
-
-    Description:
-    - This example shows how to delete a user from the database.
-
-    Steps:
-    1. Pass the user_id params of the user you want to delete.
-    2. After getting response, check your mongodb database to see if the user is deleted.
-    */
-
+    
     let { user_id } = this;
 
     const [userLib] = AutoLoad.loadLibray("mongoLib", ["user"]);
