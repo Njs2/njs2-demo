91b36f477f07705f106d8a4e21a69c18c93f296a
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 11:01:00 +0530

    Update action.js

--- src/methods/example.sqlTransactionQuery/action.js
+++ src/methods/example.sqlTransactionQuery/action.js
@@ -1,18 +1,6 @@
 
 class ExampleSqlTransactionQueryAction extends baseAction {
-    /*
-    SQL TRANSACTION EXAMPLE
 
-    Description:
-    - This example shows how to implement transaction queries.
-
-    Steps:
-    1. Pass the following arguments:
-      * user_id
-      * status
-    2. Sucess response will be returned if transaction is sucess else a Failed response is returned.
-
-    */
   async executeMethod() {
     let { userId,status } = this;
     
