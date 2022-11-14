f8e745c9700f403340705b8a55ca57dbc84572d2
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 10:41:37 +0530

    Update action.js

--- src/methods/example.redisGet/action.js
+++ src/methods/example.redisGet/action.js
@@ -1,16 +1,6 @@
 class ExampleRedisGetAction extends baseAction {
   async executeMethod() {
-    /*
-    REDIS GET/READ DATA EXAMPLE
-
-    Description:
-    - This example shows how to read/get data from redis.
-
-    Steps:
-    1. Select example/redisGet in postman under "demo-project" collection.
-    2. Pass the user_id as an argument
-    */
-
+    
     let { user_id } = this;
     let user;
     user = await REDISManager.getKey(user_id);
