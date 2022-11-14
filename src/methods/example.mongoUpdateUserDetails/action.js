b38da01eab9d2e1f27890fe0f480029d99a5148d
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 10:39:18 +0530

    Update action.js

--- src/methods/example.mongoUpdateUserDetails/action.js
+++ src/methods/example.mongoUpdateUserDetails/action.js
@@ -1,19 +1,6 @@
 class ExampleMongoUpdateUserDetailsAction extends baseAction {
   async executeMethod() {
-    /*
-    MONGO-DB UPDATE USER EXAMPLE
-
-    Description:
-    - This example shows how to update a user's name.
-
-    Steps:
-    1. In postman, go to: example/mongoUpdateUserDetails.
-    2. Pass the following parameters:
-      - user_id: The user's id.
-      - updated_user_name: The new name.
-    3. After getting response, check your mongodb database to see if the user is updated.
-    */
-
+    
     let { user_id, updated_user_name } = this;
     const [userLib] = AutoLoad.loadLibray("mongoLib", ["user"]);
 
