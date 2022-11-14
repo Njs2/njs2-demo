24509715727e4a2afe8fa562d5e1a9af85a0f069
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 10:48:28 +0530

    Update action.js

--- src/methods/example.socketDisconnect/action.js
+++ src/methods/example.socketDisconnect/action.js
@@ -1,15 +1,6 @@
 class ExampleSocketDisconnectAction extends baseAction {
   async executeMethod() {
-    /*
-    SOCKET DISCONNECT EXAMPLE
-
-    Steps:
-    1. This will execute when the socket is disconnected.
-    2. In this example, we expects the socket_id of socket disconnection (Read init.js for setup).
-    3. On diconnection event socket id will be removed from the sockets list, i.e SOCKET_ID_ARRAY.
-    4. Then Broadcast message will send to all the remaining sockets. 
-    */
-
+    
     let { socket_id } = this;
 
     //getting index of disconnected socket id
