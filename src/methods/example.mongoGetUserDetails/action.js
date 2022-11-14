51781b7400ae03f536d1196279a046d22bf34ae7
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 10:36:35 +0530

    Update action.js

--- src/methods/example.mongoGetUserDetails/action.js
+++ src/methods/example.mongoGetUserDetails/action.js
@@ -1,16 +1,6 @@
 class ExampleMongoGetUserDetailsAction extends baseAction {
   async executeMethod() {
-    /*
-    MONGO-DB READ/GET USER EXAMPLE
-
-    Description:
-    - This example shows how to read/get a user from the database.
-
-    Steps:
-    1. In postman, go to: example/mongoGetUserDetails.
-    2. Pass the user_id as a parameter.
-    */
-
+    
     let { user_id } = this;
     const [userLib] = AutoLoad.loadLibray("mongoLib", ["user"]);
 
