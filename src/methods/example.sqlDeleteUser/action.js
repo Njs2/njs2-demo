ec83807573f7da507b40260fec83d5083ad88591
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 10:59:00 +0530

    Update action.js

--- src/methods/example.sqlDeleteUser/action.js
+++ src/methods/example.sqlDeleteUser/action.js
@@ -1,18 +1,6 @@
 class ExampleSqlDeleteUserAction extends baseAction {
   async executeMethod() {
-    /*
-    SQL DELETE USER EXAMPLE
-
-    Description:
-    - This example shows how to delete a user in the sql database.
-
-    Steps:
-    1. Pass following arguments:
-      * user_id
-    2. After getting response, check your database to see if user was deleted.
-
-    */
-
+    
     const [userLib] = AutoLoad.loadLibray("sqlLib", ["user"]);
     let { user_id } = this;
 
