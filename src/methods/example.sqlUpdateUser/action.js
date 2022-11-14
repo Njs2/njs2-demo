f9188217cd8dac5296c6c16cf0c2b11dfcf9b582
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 11:01:58 +0530

    Update action.js

--- src/methods/example.sqlUpdateUser/action.js
+++ src/methods/example.sqlUpdateUser/action.js
@@ -1,20 +1,6 @@
 class ExampleSqlUpdateUserAction extends baseAction {
   async executeMethod() {
-    /*
-    SQL UPDATE USER EXAMPLE
-
-    Description:
-    - This example shows how to update a user in the sql database.
-
-    Steps:
-    1. Pass following arguments in params or request body:
-      * user_id
-      * user_name (for update)
-      * gender (for update)
-    2. After getting response, check your database.
-
-    */
-
+    
     const [userLib] = AutoLoad.loadLibray("sqlLib", ["user"]);
     let { user_name, gender, user_id } = this;
 
