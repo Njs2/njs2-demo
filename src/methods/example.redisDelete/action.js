fad025f976b96bd213d7281103ac29539f21ae6e
ganeshsacharya <60443722+ganeshsacharya@users.noreply.github.com>
2022-11-14 10:40:24 +0530

    Update action.js

--- src/methods/example.redisDelete/action.js
+++ src/methods/example.redisDelete/action.js
@@ -1,16 +1,6 @@
 class ExampleRedisDeleteAction extends baseAction {
   async executeMethod() {
-    /*
-    REDIS DELETE DATA EXAMPLE
-
-    Description:
-    - This example shows how to delete data from redis.
-
-    Steps:
-    1. Select example/redisDelete in postman under "demo-project" collection.
-    2. Pass the user_id as an argument.
-    */
-
+    
     let { user_id } = this;
     const result = await REDISManager.deleteKey(user_id);
 
